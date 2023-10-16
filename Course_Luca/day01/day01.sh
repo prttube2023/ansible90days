e191c066fe46b87256d7c6c3571df7cc94b9d86c
preetam_tube <preetam.tube@gmail.com>
2023-10-16 19:31:30 +0000

    added day 2 files

--- Course_Luca/day01/day01.sh
+++ Course_Luca/day01/day01.sh
@@ -3,3 +3,6 @@ ansible -m ping localhost
 
 # get facts of any machine, you can substite localhost with your own host/IP Address
 ansible -m setup localhost
+
+# enter the variables on the fly using --extra-vars="nameofthevariable=valueofthevariable"
+ansible-playbook -i inventory --extra-vars="timetowait=5" playbook-pause.yaml
